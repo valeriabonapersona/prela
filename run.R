7b808dab0ddb8019e8f96018ed98f0667a771d73
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-10-14 16:54:14 +0200

    updated with eff size behavior

--- run.R
+++ run.R
@@ -1,6 +1,9 @@
 ## files to run
 
 ### From behavior data (mab project), select that of interest for this study
+## output in raw (mab.RDS)
 source("src/mab_xl_to_rds.R")
 
-### from mab data, get estimates
+### from mab data, get eff size estimates and aggregates wherever necessary
+## output in temp (mab_aggr.RDS)
+source("src/mab_effsize.RDS")

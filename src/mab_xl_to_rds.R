b3de2dc5ccf62128c4aaa11d8c3a18f56316e200
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-10-14 16:35:11 +0200

    from xl to rds for behavior data

---
+++ src/mab_xl_to_rds.R
@@ -0,0 +1,136 @@
+'
+  Script: Organize data on behavior
+  Input: messy xl file with manual extraction
+  Output: .csv and .RDS files harmonized (before preprocessing)
+'
+
+# Environment preparation -------------------------------------------------
+source("config/utilities.R")
+source("src/general_funs.R")
+
+# upload data
+temp_path <- "~/surfdrive/Work/PhD/mELA_materials/Eline/all_files_mELA_2020/5. Files/outcomes_mELA_VB_v3.xlsx"
+dat_xl <- read_my_sheets(temp_path, c("publications", "experiments", "out_behavior"))
+
+# Select data of interest -------------------------------------------------
+'
+  Data of interest is in the out_behavior tab under "prela". So far we have included
+  the hippocampal dependent tasks object in context and object in location, as well as fear conditioning (test phase).
+  We still need to discuss whether to include the MWM, as its interpretation changes based on the water temperature
+'
+mab <- dat_xl$out_behavior %>% 
+  
+  # only hip dependent (and processed)
+  filter(prela == "yes") %>% 
+  
+  # get exp and paper info
+  left_join(dat_xl$experiments, by = "exp_id") %>% 
+  left_join(dat_xl$publications %>% 
+              rename(id = PMID) %>% 
+              dplyr::select(-c("CHECK WITH EXP", starts_with("..."))), 
+            by = "id") %>% 
+  
+  # create missing vars
+  mutate(
+    # for citation
+    cite = paste0(word(authors, 1), "(", year,")"),
+    cite = str_replace_all(cite, ",", ""),
+    
+    # for info about model
+    model_postdays = ifelse(model_start != model_end, 
+                            paste(model_start, model_end, sep = "-"), 
+                            model_start),
+    separation_light_phase = ifelse(separation_light_phase == "no", "not_applicable", 
+                                    separation_light_phase), 
+    housing_after_weaning = ifelse(str_detect(housing_after_weaning.x, "pair"), 
+                                   housing_after_weaning.x, housing_after_weaning.y), 
+    housing_after_weaning = ifelse(is.na(housing_after_weaning), 
+                                   housing_after_weaning.y, housing_after_weaning), 
+    age_testing_start = str_remove_all(age_testing_start, "not_specified"), 
+    age_testing_end = str_remove_all(age_testing_end, "not_specified"), 
+    sex = case_when(
+      sex == "M" ~ "male", 
+      sex == "F" ~ "female", 
+      T ~ sex
+    ), 
+    retention_hours = ifelse(retention_to_test_unit == "min", 
+                             as.numeric(retention_to_test) / 60, retention_to_test), 
+    habituation = case_when(
+      habituation %in% c("not_applicable") ~ "not_specified", 
+      str_detect(habituation, "room") ~ "room", 
+      str_detect(habituation, "arena") ~ "arena", 
+      T ~ habituation
+    ), 
+    cut_n_c = ifelse(is.na(cut_n_c), "no", cut_n_c)
+  ) %>% 
+  
+  # create var for age testing - get mean of start and end, convert to weeks - put together
+  separate(age_testing_start, into = c("start_1", "start_2"), sep = "_")  %>% 
+  separate(age_testing_end, into = c("end_1", "end_2"), sep = "_")  %>% 
+  
+  mutate(
+    
+    # get mean for ranges
+    across(starts_with("start"), as.numeric),
+    start = ifelse(is.na(start_2), start_1, start_1 + start_2/2),
+    
+    across(starts_with("end"), as.numeric),
+    end = ifelse(is.na(end_2), end_1, end_1 + end_2/2),
+    
+    # convert to weeks
+    start = case_when(
+      age_testing_unit.y == "months" ~ start * 4,
+      age_testing_unit.y == "days" ~ start / 7,
+      T ~ start
+    ),
+    end = case_when(
+      age_testing_unit.y == "months" ~ end * 4,
+      age_testing_unit.y == "days" ~ end / 7,
+      T ~ end
+    ), 
+    
+    # put together
+    age_weeks = case_when(
+      is.na(start) ~ "", 
+      is.na(end) ~ as.character(round(start)), 
+      T ~ paste(round(start), round(end), sep = "-")
+    )
+  ) %>%
+
+  rename(
+    separation_hours = seperation_time,
+    
+    # all estimates have the same type
+    mean_c = estimate_c,
+    mean_e = estimate_e,
+    sem_c = deviation_c,
+    sem_e = deviation_e
+    ) %>%
+  
+  # keep only vars of interest
+  select(
+    # about publications
+    cite, link, id, title, exp_id, outcome_id,
+    
+    # about the population
+    species, strain, origin, sex,
+    
+    # about the intervention
+    model, model_control, model_postdays, separation_hours,
+    separation_repetition, separation_light_phase, 
+    cross_fostering, litter_size, age_weeks, 
+    
+    # about the outcome
+    outcome, retention_hours, light_dark_phase, handling, 
+    habituation, comment_behavior, 
+    
+    # statistics
+    cut_n_c, ends_with("_c"), ends_with("_e"), data_unit
+  )
+
+
+
+
+# Save --------------------------------------------------------------------
+saveRDS(mab, paste(raw,"mab.RDS"))
+write.csv(mab, paste(raw,"mab.csv"))

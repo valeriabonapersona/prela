3d22214c79e647191ae7afdd31a022702f6f6c87
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-10-15 10:14:09 +0200

    typo

--- run.R
+++ run.R
@@ -6,4 +6,4 @@ source("src/mab_xl_to_rds.R")
 
 ### from mab data, get eff size estimates and aggregates wherever necessary
 ## output in temp (mab_aggr.RDS)
-source("src/mab_effsize.RDS")
+source("src/mab_effsize.R")

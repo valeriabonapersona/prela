dbeda3fede38f91d7e73965b1e05459c13bc053a
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-11-02 18:37:33 +0100

    updated with merge data

--- run.R
+++ run.R
@@ -6,3 +6,6 @@ source("src/mab_xl_to_rds.R")
 
 ### from excel sheet, organized data on publications and experiments
 source("src/exp_xls_to_rds.R")
+
+### merge all data from prela. Outputs in temp an unprocessed prela file (prela_data_unprocessed.RDS)
+source("src/merge_data.R")

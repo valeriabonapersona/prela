3c3c46efa1b04a70d2b74de97d88f27aa0759e9f
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-11-04 14:29:53 +0100

    harmonization brain areas and summ stats

--- src/merge_data.R
+++ src/merge_data.R
@@ -56,6 +56,110 @@ all_data <- exp %>%
 #   pull(exp_id)
 
 
+# Harmonization across datasets -------------------------------------------
+# change other in strain with actual strain?
+all_data <- all_data %>% 
+  
+  mutate(
+    remove_analysis = ifelse(is.na(remove_analysis), "no", remove_analysis),
+    at_death = ifelse(
+      at_death %in% c("novel Shock in shock-probe burial task", "3h after stressful physical stimulation"), "stressed",
+      remove_analysis
+    ), 
+    domain = ifelse(outcome_id == "22922490_5_GLUT_1", "functional_gaba_glut", domain),
+    ba_grouped = case_when(
+      str_detect(brain_area_publication, "striatum|nucleus_accumb|pallid|caud|septum|bed_nucleus") ~ "striatum_and_pallidum",
+      str_detect(brain_area_publication, "nucleus_accumb") ~ "nucleus_accumbens",
+      str_detect(brain_area_publication, "caud") ~ "caudate_putamen",
+      
+      
+      str_detect(brain_area_publication, "hippocamp|dentate|GZ|subiculum") ~ "hippocampal_region",
+      brain_area_publication == "subventricular_zone" ~ "hippocampal_region",
+      str_detect(brain_area_publication, "amygda") ~ "amygdala", 
+      str_detect(brain_area_publication, "frontal") ~ "prefrontal_cortex",
+      str_detect(brain_area_publication, "hypothal|mammilary|suprachiasmatic|incerta|optic|mammillary") ~ "hypothalamic_region",
+      
+      
+      str_detect(brain_area_publication, "thalam|habenula|paraventricular_nucleus") ~ "thalamic_region",
+      str_detect(brain_area_publication, "tegmental|substan|") ~ "vta_substancianigra",
+      str_detect(brain_area_publication, "gray|raphe|midbrain|medulla|pons|brainstem|colliculus|tractus_solitarius|pontine") ~ "brainstem_midbrain_hindbrain",
+      
+      str_detect(brain_area_publication, "cerebellum") ~ "cerebellum",
+      #   str_detect(brain_area_publication, "olfact") ~ "olfactory_areas",
+      
+      str_detect(brain_area_publication, "ventricles|edinger|internal_capsule|cortex|endopiri|olfact|cortical_layer") ~ "other_areas",
+      #  str_detect(brain_area_publication, "cortex") ~ "cortex_other",
+      #   str_detect(brain_area_publication, "medulla|pons|brainstem") ~ "brainstem",
+      #  str_detect(brain_area_publication, "colliculus") ~ "colliculus",
+      
+      T ~ brain_area_publication
+    ), 
+    
+    ba_main = case_when(
+      str_detect(brain_area_publication, "striatum") ~ "striatum",
+      str_detect(brain_area_publication, "nucleus_accumb") ~ "nucleus_accumbens",
+      str_detect(brain_area_publication, "pallid") ~ "pallidum",
+      str_detect(brain_area_publication, "caud") ~ "caudate_putamen",
+      str_detect(brain_area_publication, "substant") ~ "sub_nigra",
+      str_detect(brain_area_publication, "tegment") ~ "vta",
+      
+      
+      str_detect(brain_area_publication, "hippocamp|dentate|GZ") ~ "hippocampus",
+      str_detect(brain_area_publication, "amygda") ~ "amygdala", 
+      str_detect(brain_area_publication, "frontal") ~ "prefrontal_cortex",
+      str_detect(brain_area_publication, "hypothal|mammilary|suprachiasmatic|incerta|optic") ~ "hypothalamic_nuclei",
+      
+      
+      str_detect(brain_area_publication, "thalam|habenula") ~ "thalamic_nuclei",
+      str_detect(brain_area_publication, "raphe|midbrain|colliculus") ~ "midbrain",
+      str_detect(brain_area_publication, "gray|medulla|ponsbrainstem|") ~ "brainstem_and_hindbrain",
+      
+      str_detect(brain_area_publication, "olfact") ~ "olfactory_areas",
+      
+      str_detect(brain_area_publication, "ventricles|edinger|internal_capsule|endopiri") ~ "other_areas",
+      str_detect(brain_area_publication, "cortex") ~ "other_cortical",
+      #   str_detect(brain_area_publication, "medulla|pons|brainstem") ~ "brainstem",
+      #  str_detect(brain_area_publication, "colliculus") ~ "colliculus",
+      
+      T ~ brain_area_publication
+    ),
+    ba_location = case_when(
+      str_detect(brain_area_publication, "dorsal") ~ "dorsal", 
+      str_detect(brain_area_publication, "ventral") ~ "ventral", 
+      str_detect(brain_area_publication, "basal") ~ "basal", 
+      str_detect(brain_area_publication, "caudal") ~ "caudal", 
+      str_detect(brain_area_publication, "apical") ~ "apical", 
+      str_detect(brain_area_publication, "rostral") ~ "rostral", 
+      str_detect(brain_area_publication, "medial") ~ "medial", 
+      str_detect(brain_area_publication, "superficial") ~ "superficial", 
+      str_detect(brain_area_publication, "deep") ~ "deep", 
+      str_detect(brain_area_publication, "core") ~ "core", 
+      
+        T ~ "not_specified"
+   #   T ~ brain_area_publication
+    ), 
+   
+   across(c("estimate_c_type", "estimate_e_type"), 
+          function(x) {
+            case_when(
+              x %in% c("average", "arbitrary_units") ~ "mean",
+              is.na(x) ~ "not_available",
+              T ~ x
+              )}),
+   
+   across(c("deviation_c_type", "deviation_e_type"), 
+          function(x) {
+            case_when(
+              x == "SE" ~ "sem",
+              is.na(x) ~ "not_available",
+              T ~ str_to_lower(x)
+            )
+            })
+     
+  ) %>% 
+  select(-c(ba_layer, cell_type))
+
+
 
 # Save data ---------------------------------------------------------------
 saveRDS(all_data, paste0(temp, "prela_data_unprocessed.RDS"))

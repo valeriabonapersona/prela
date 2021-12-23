398b1f98185174315f7783561fe8b5968988f868
valeriabonapersona <valeria.bonapersona@gmail.com>
2021-12-23 22:45:38 +0100

    cvr

--- src/upload_data.R
+++ src/upload_data.R
@@ -23,6 +23,35 @@ dat <- dat %>%
 dat_full <- dat_full %>% 
   mutate(strain = ifelse(strain %in% c("c3hhenrj", "swiss_webster"), "other", strain))
 
+# separate data for cvr
+dat_cvr <- dat %>%
+  select(-c(yi, vi)) %>% 
+  rename(yi = yi_cvr, vi = vi_cvr) %>% 
+  mutate(
+    origin_num = ifelse(origin == "purchased_pregnant_dams", 1, 0), 
+    major_life_events_num = ifelse(major_life_events == "yes", 1, 0), 
+    behavior_num = ifelse(behavior == "stressful", 1, 0)
+  ) %>% 
+  rowwise() %>% 
+  mutate(
+    trauma_score = factor(sum(origin_num, major_life_events_num, behavior_num), levels = c("0", "1", "2", "3")),
+    trauma_score = paste0("+", trauma_score)
+  ) %>% 
+  select(-c(ends_with("_num"))) %>%
+  mutate(
+    life_exp = case_when(
+      trauma_score == "+0" & behavior %in% c("naive", "no_behavior") ~ "naive", 
+      trauma_score == "+0" & behavior %in% c("non_stressful") ~ "non_stressful", 
+      T ~ paste(trauma_score, "hit")
+    )
+  ) %>% 
+  filter(!is.na(yi))
 
 # Experiments' information ------------------------------------------------
 exp <- readRDS(paste0(temp, "info_experiments.RDS"))
+
+
+# Select only limbic ------------------------------------------------------
+limbic <- dat %>% 
+  filter(ba_grouped %in% c("amygdala", "prefrontal_cortex", "hippocampal_region"))
+
